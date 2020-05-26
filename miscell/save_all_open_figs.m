bbc675ead1deec699addd872af8d02962dbac25f
Armando Longobardi <armando.longobardi@pirelli.com>
2020-05-26 17:59:28 +0200

    save_all_open_figs converted to a function
    Added option to save also in others format (jpg,png) and for closing or not the windows

--- miscell/save_all_open_figs.m
+++ miscell/save_all_open_figs.m
@@ -1,11 +1,35 @@
+function save_all_open_figs(save_folder,close_check,ext)
+
+if nargin<1
+    close_check=false;
+    save_folder=pwd;
+    ext=[];
+elseif nargin<2
+    close_check=false;
+    ext=[];
+elseif nargin<3
+    ext=[];
+end
+
 hands   = get (0,'Children');   % locate fall open figure handles
 hands   = sort(hands);          % sort figure handles
 numfigs = size(hands,1);        % number of open figures
 
-save_folder='C:\Users\longoar001\Desktop\AL_desk\Topics\14_DMA\figures\PSN\analisi-giri concatenati e hannati';
-
 for i_fig=1:numfigs
-    savefig(hands(i_fig),[save_folder,filesep,hands(i_fig).Name,'_',datestr(datetime,'yyyymmdd_HHMMSS')])
+    if isempty(ext)
+        savefig(hands(i_fig),[save_folder,filesep,hands(i_fig).Name,'_',datestr(datetime,'yyyymmdd_HHMMSS')],'compact')
+    else
+        try
+            saveas(hands(i_fig),[save_folder,filesep,hands(i_fig).Name,'_',datestr(datetime,'yyyymmdd_HHMMSS')],ext)
+        catch
+            warning('Extension not valid. /n Function interrupted')
+            return
+        end
+    end
 end
 
-% close all
+if close_check
+    close all
+end
+
+end
