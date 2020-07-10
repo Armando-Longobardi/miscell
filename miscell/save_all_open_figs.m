596bec528f39fe77e47aee07f594b3f40a5bf0a0
Armando Longobardi <armando.longobardi@pirelli.com>
2020-07-10 08:33:08 +0200

    save_all_open_figs - function call changed
    Now with parameters, other than save_folder

--- miscell/save_all_open_figs.m
+++ miscell/save_all_open_figs.m
@@ -1,14 +1,25 @@
-function save_all_open_figs(save_folder,close_check,ext)
+function save_all_open_figs(save_folder,varargin)
 
 if nargin<1
-    close_check=false;
-    save_folder=pwd;
-    ext=[];
-elseif nargin<2
+    save_folder=uigetdir;
+end
     close_check=false;
     ext=[];
-elseif nargin<3
-    ext=[];
+    nameOpt = 'figName';
+    corrName=false;
+    
+    Props={'CloseAll','SaveAs','NameAs','CorrectName'};
+for iInput= 1:2:numel(varargin)
+    switch validatestring(varargin{iInput},Props)
+        case 'CloseAll'
+            close_check=varargin{iInput+1};
+        case 'SaveAs'
+            ext=varargin{iInput+1};
+        case 'NameAs'
+            nameOpt=validatestring(varargin{iInput+1},{'figName','Title'});
+        case 'CorrectName'
+            corrName=varargin{iInput+1};
+    end
 end
 
 hands   = get (0,'Children');   % locate fall open figure handles
@@ -17,10 +28,24 @@ numfigs = size(hands,1);        % number of open figures
 
 for i_fig=1:numfigs
     if isempty(ext)
+        switch nameOpt
+            case 'figName'
+                partial_name=hands(i_fig).Name;
+            case 'Title'
+                Axs=findobj(hands(i_fig),'Type','Axes');
+                partial_name=Axs.Title.String';
+                partial_name=regexprep(partial_name(:)',' *','_');
+        end
+        
+        if corrName
+            partial_name=correct_name(partial_name,50);
+        end
+        
+        figname=[save_folder,filesep,partial_name,'_',datestr(datetime,'yyyymmdd_HHMMSS')];
+                
         
         
-        
-        savefig(hands(i_fig),[save_folder,filesep,correct_name(hands(i_fig).Name,50),'_',datestr(datetime,'yyyymmdd_HHMMSS')],'compact')
+        savefig(hands(i_fig),figname,'compact')
     else
         try
             saveas(hands(i_fig),[save_folder,filesep,correct_name(hands(i_fig).Name,50),'_',datestr(datetime,'yyyymmdd_HHMMSS')],ext)
