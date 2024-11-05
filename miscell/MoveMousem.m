7f5fcb73c4ee5cfd6d42fd597a2f019b3dff780c
ArmandoLongobardi <armando_longobardi@goodyear.com>
2024-11-05 13:47:36 +0100

    Add utility script for screen inactivity

---
+++ miscell/MoveMousem.m
@@ -0,0 +1,14 @@
+import java.awt.Robot;
+import java.awt.event.*;
+mouse = Robot;
+% pause(60*60*3)
+while 1
+mouse.mouseMove(100,100)
+pause(2)
+mouse.mouseMove(200,200)
+pause(2)
+mouse.mouseMove(100,100)
+pause(2)
+mouse.mouseMove(200,200)
+pause(60)
+end
